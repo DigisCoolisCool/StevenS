d53e8e4c9c09de29fb33fad26162a9d236d83ade
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2022-12-01 05:23:34 -0800

    1.0

---
+++ FPS NSFW  steven/Assets/Scenes/ManagerPC.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class ManagerPC : MonoBehaviour
+{
+
+    public int health;
+    public GameObject Lastsmoke;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0) DestroyNPC();
+      
+    }
+    public  void GotHit()
+    {
+        health -= 25;
+}
+        
+    public void DestroyNPC()
+    {
+        GameObject Smoke = Instantiate(Lastsmoke, transform.position, Quaternion.identity);
+        Destroy(Smoke, 1);
+        Destroy(gameObject);
+    }
+
+}
---
+++ FPS NSFW  steven/Assets/Scenes/Playerscript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerscript : MonoBehaviour
+{
+    Camera Playercamera;
+    Ray rayFromPlayer;
+    RaycastHit hit;
+    public GameObject sparkles;
+    public int ammo = 10;
+    public ParticleSystem muzzleFlash;
+   // public AudioSource Lasersound;
+   // public AudioSource ammopickup;
+        // Start is called before the first frame update
+    void Start()
+    {
+        Playercamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rayFromPlayer = Playercamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        Debug.DrawRay(rayFromPlayer.origin, rayFromPlayer.direction * 100, Color.red);
+        if (Input.GetKeyDown(KeyCode.E))
+            {
+            if(Physics.Raycast(rayFromPlayer,out hit, 100) && ammo > 0)
+            {
+                print("The object is" + hit.collider.gameObject.name + "it front of player");
+                Vector3 positionoFimpact;
+                positionoFimpact = hit.point;
+                    Instantiate(sparkles, positionoFimpact, Quaternion.identity);
+                GameObject objectTargerted;
+               muzzleFlash.Play();
+
+                
+               
+                if (hit.collider.gameObject.tag == "Target")
+                    {
+                    objectTargerted = hit.collider.gameObject;
+                    objectTargerted.GetComponent<ManagerPC>().GotHit();
+                }
+
+            }
+            ammo--;
+            print("you have " + ammo + "ammo left");
+            //Lasersound.Play();
+        } 
+    }
+
+    public void ManageCollisions(ControllerColliderHit hit)
+    {
+        if (hit.collider.gameObject.tag == "ammogun")
+        {
+            ammo += 5;
+            if (ammo > 25) ammo = 25;
+            Destroy(hit.collider.gameObject);
+            //ammopickup.Play();
+        }
+    }
+}
